576e369c4719bde757202620adc993cd51f2f6e2
jakabus <jakabus@gmail.com>
2018-06-22 12:07:09 +0200

    second part of wordfreq (gyak)

---
+++ week-06/day-04/Todo/Todo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Todo;
+
+namespace Todo
+{
+    class Program
+    {
+        private static List<TodoClass> todos = new List<TodoClass>();
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                todos.Add(new TodoClass(i, "asd", DateTime.Now));
+            }
+        }
+
+        private static void Add(TodoClass todo)
+        {
+            todos.Add(todo);
+        }
+    }
+}
---
+++ week-06/day-04/Todo/Todo/Todo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo
+{
+    public class TodoClass
+    {
+        private int id;
+        private string text;
+        private DateTime dateTime;
+
+        public TodoClass(int id, string text, DateTime dateTime)
+        {
+            this.Id = id;
+            this.Text = text;
+            this.DateTime = dateTime;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Text { get => text; set => text = value; }
+        public DateTime DateTime { get => dateTime; set => dateTime = value; }
+    }
+}
