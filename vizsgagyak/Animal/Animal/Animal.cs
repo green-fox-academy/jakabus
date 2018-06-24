c9c25902c566a17ddf1cab3e0fd0a471b9671255
jakabus <jakabus@gmail.com>
2018-06-24 14:27:53 +0200

    allat vagyok

---
+++ vizsgagyak/Animal/Animal/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    class Animal
+    {
+        private int hunger;
+        private int thirst;
+
+        public Animal()
+        {
+            this.hunger = 50;
+            this.thirst = 50;
+        }
+
+        public void Eat()
+        {
+            hunger--;
+        }
+        public void Drink()
+        {
+            thirst--;
+        }
+        public void Play()
+        {
+            hunger++;
+            thirst++;
+        }
+    }
+}
