7a806b825646cea9ac6f6fd5f2a6b456b53c6d03
jakabus <jakabus@gmail.com>
2018-06-25 16:44:39 +0200

    views

---
+++ week-07/day-01/FirstCoreApp/FirstCoreApp/Controllers/WebController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreApp.Controllers
+{
+    [Route("web")]
+    public class WebController : Controller
+    {
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            var greeting = new Greeting()
+            {
+                Id = 1,
+                Content = "I'am a hacker"
+            };
+
+            return View(greeting);
+        }
+
+    }
+}
