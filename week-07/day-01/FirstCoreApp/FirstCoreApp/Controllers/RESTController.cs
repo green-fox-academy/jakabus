5d6133d3fa7f703e9445d8841e3be1156cbc0a40
jakabus <jakabus@gmail.com>
2018-06-25 15:00:12 +0200

    eljutottamidaigcsakelakadtamessegitsegrevarokezrtismegkertekisgondoltamcommitolok

---
+++ week-07/day-01/FirstCoreApp/FirstCoreApp/Controllers/RESTController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreApp.Controllers
+{
+    public class RESTController : Controller
+    {   
+        [Route("api")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            return new JsonResult("Greeting");
+        }
+    }
+}
---
+++ week-07/day-01/FirstCoreApp/FirstCoreApp/Models/Greeting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreApp.Models
+{
+    public class Greeting
+    {
+        private long Id;
+        private string Content;
+
+    }
+}
