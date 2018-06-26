527260044b973fa35564a87cf9ac950d718b5839
jakabus <jakabus@gmail.com>
2018-06-26 18:18:09 +0200

    ugh visszafejlodtemkicsit de nem allit meg

---
+++ week-07/day-02/BankOfSimba/BankOfSimba/Controllers/BankAccController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimba.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankOfSimba.Controllers
+{
+    public class BankAccController : Controller
+    {
+        List<BankAccount> Accounts = new List<BankAccount>();
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Simba()
+        {
+            var bankAcc = new BankAccount("Jason", 1000, "allat");
+
+            return null;
+        }
+    }
+}
---
+++ week-07/day-02/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public string AnimalType { get; set; }
+
+
+        public BankAccount(string name, int balance, string animalType)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+        }
+    }
+}
