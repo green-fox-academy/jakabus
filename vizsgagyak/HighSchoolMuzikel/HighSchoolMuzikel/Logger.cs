46e24a8d13a8dbc5805fdab8f9152f151d0b08c6
jakabus <jakabus@gmail.com>
2018-06-26 18:22:57 +0200

    feat(hs) logger mechanism

--- vizsgagyak/HighSchoolMuzikel/HighSchoolMuzikel/Logger.cs
+++ vizsgagyak/HighSchoolMuzikel/HighSchoolMuzikel/Logger.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace HighSchoolMuzikel
 {
     class Logger
     {
+        static string filename = "log.txt";
+
+        public static void LogLine(string line)
+        {
+            try
+            {
+                List<string> lineToAppend = new List<string>() 
+                { line };
+                File.AppendAllLines(filename,lineToAppend);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
