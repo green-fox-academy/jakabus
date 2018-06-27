4053a9d3576c9289e7a8a9f38f2e191187505b70
jakabus <jakabus@gmail.com>
2018-06-27 12:57:03 +0200

    12:56

---
+++ vizsgagyak/TeljesVizsgaGyakRetake/NthElementWTest/NthElementWTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NthElementWTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 4, 10, 34, 3, 16, 3, 21 };
+
+        }
+
+        public void NthElement(int[] numbers, int n)
+        {
+
+        }
+    }
+}
