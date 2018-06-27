fb4e896b154c8da4e58ec79be9c772ac1f2fc6d4
jakabus <jakabus@gmail.com>
2018-06-27 15:46:51 +0200

    asd

---
+++ vizsgagyak/TeljesVizsgaGyakRetake/WordFreq/WordFreq/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WordFreq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "input.txt";
+            foreach (string word in FileRead(filename, 2))
+            {
+                Console.WriteLine("{0}", word);
+            }
+            
+
+            Console.ReadLine();
+        }
+
+        public static List<string> FileRead(string filename, int freq)
+        {
+            string[] lines;
+            try
+            {
+                lines = File.ReadAllLines(filename);
+            }
+            catch (FileNotFoundException)
+            {
+                lines = new string[0];
+                Console.WriteLine("No such File!");
+            }
+
+            List<string> words = new List<string>();
+
+            foreach (string line in lines)
+            {
+                foreach (string word in line.Split(' '))
+                {
+                    words.Add(word);
+                }
+            }
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (!dict.ContainsKey(word))
+                {
+                    dict[word] = 1;
+                }
+                else
+                {
+                    dict[word]++;
+                }
+            }
+
+            List<string> finalWords = new List<string>();
+
+            foreach (KeyValuePair<string, int> word in dict)
+            {
+                if (word.Value==freq)
+                {
+                    finalWords.Add(word.Key);
+                }
+            }
+
+            return finalWords;
+        }
+    }
+}
