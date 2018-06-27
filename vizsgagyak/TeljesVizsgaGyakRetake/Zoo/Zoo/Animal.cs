dbea1c49e27576ce9034c4e57b252d602212b1aa
jakabus <jakabus@gmail.com>
2018-06-27 16:20:02 +0200

    kezdetek2

--- vizsgagyak/TeljesVizsgaGyakRetake/Zoo/Zoo/Animal.cs
+++ vizsgagyak/TeljesVizsgaGyakRetake/Zoo/Zoo/Animal.cs
@@ -6,5 +6,39 @@ namespace Zoo
 {
     class Animal
     {
+        private string name;
+        private int age;
+        private string gender;
+        private int fedTimes;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.fedTimes = 0;
+        }
+        public Animal(string name)
+        {
+            this.name = name;
+            this.age = 0;
+            this.gender = "unknown";
+            this.fedTimes = 0;
+        }
+
+        public void Eat()
+        {
+            fedTimes++;
+        }
+
+        public void IsHungry()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
