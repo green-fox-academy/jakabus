f4d8a97077f1517594deb8f0b074bac3fa5ec323
jakabus <jakabus@gmail.com>
2018-07-03 16:41:11 +0200

    i need explanation

---
+++ week-08/day-02/ListingTodos/ListingTodos/Controllers/TodoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListingTodos.Controllers
+{
+    [Route("/todo")]
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/list")]
+        public IActionResult List()
+        {
+            return Content("This is my first mindfuck of the day yey!");
+        }
+    }
+}
---
+++ week-08/day-02/ListingTodos/ListingTodos/Repositories/InjectionClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingTodos.Repositories
+{
+    public class InjectionClass : TodoContext
+    {
+        private TodoContext todoContext;
+
+        public InjectionClass(TodoContext todoContext)
+        {
+            this.todoContext = todoContext;
+        }
+    }
+}
